60907437458be1254fa09c0742414b87425db577
AdiPalguna <yandeadi86@gmail.com>
2025-06-11 15:01:09 +0800

    update

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -11,11 +11,11 @@ function Dashboard() {
                 <div className="">
                     <div className="text-color-1">
                         <p className="font-light text-6xl">WELCOME TO</p>
-                        <p className="text-9xl lg:mt-10">20DETIK</p>
+                        {/* <p className="text-9xl lg:mt-10">20DETIK</p> */}
                         <p className="text-[85px]">VIDEO ANALYTICS</p>
                     </div>
 
-                    <div className="flex lg:mt-20">
+                    <div className="flex lg:mt-40">
                         <div className='text-center'>
                             <button className='home-button border border-color-1 rounded-full xxs:px-2 xxs:py-1 xxs:text-sm sm:px-4 sm:text-base lg:px-6 lg:py-2 lg:mr-3'><a href="/video-analysis">Video Analytics</a></button>
                         </div>
